3eacb5f2a4ab26158241b1a839951f42b23b3796
Cedric Sumner <cedricsumner@gmail.com>
2023-10-06 13:10:41 -0400

    Add change grid functionality
    We added a button to allow the user to reset the
    number of squares in the grid, and gave it an upper
    limit of 100, so you wont crash your comp doing it.
    If you go past the warning message and put a too large
    number into it again, it'll let you. Probbably should be
    bound by some "while(isTooBig=true) function but whatever.

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let defaultValue=16;
 let containerDiv= document.querySelector('.containerDiv')
 let makeGrid=function(){
 
-
 for (let i=0;i<defaultValue;i++){
    
     let rowDiv = document.createElement('div')
@@ -20,17 +19,40 @@ for (let i=0;i<defaultValue;i++){
     }
     containerDiv.appendChild(rowDiv)
 }
-    
-
+  
+containerDiv.appendChild(button)
 }
-/*
-let button = document.createElement('button')
-button.addEventListener('click',newGrid)
 
 let newGrid = function(){
-    console.log()
+    defaultValue=prompt('enter a value','16')
+    let isTooBig = false
+    let containerKids = containerDiv.children;
+    for (let i=0;i<containerKids.length;i++){
+        console.log(containerKids.length)
+        let firstGone = containerKids[i]
+        containerDiv.removeChild(firstGone)
+    }
+    let rowKids = document.querySelectorAll('.rowDiv')
+    for (let i=0;i<rowKids.length;i++){
+        console.log(rowKids.length)
+        let secondGone = rowKids[i]
+        containerDiv.removeChild(secondGone)
+    }
+    defaultValue= +defaultValue
+    if(defaultValue>100){
+        alert ('please choose a smaller number for your comp lmao')
+        defaultValue = prompt('enter another value.')
+    }
+    
+    makeGrid();
 }
-*/
+
+let button = document.createElement('button')
+button.textContent = 'click me'
+button.addEventListener('click',newGrid)
+
+
+
 
 
 
